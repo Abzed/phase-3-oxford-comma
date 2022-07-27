148ae473cc09a12a7952b2a9edd1015b7bc43eb3
Abzed <ibraabzed@gmail.com>
2022-07-27 11:19:22 +0300

    commit tree 6c1f0ccf801c952f48408608bfc5f979ecb89b20

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
+require "pry"
 
+def oxford_comma(array)
+    if array.size() == 1
+        array.join()
+    elsif array.size() == 2
+        array.join(" and ")
+    elsif array.size >= 3
+        newArr = array.join(", ")
+        [newArr]
+    end
 end
+
+
